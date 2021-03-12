c61db666eecfec6f03aeb7a834ce60478e867cc9
Ghazanfar-ijaz <ghazanfar@outlook.fr>
2021-03-12 10:46:17 +0100

    test

--- exo-fonctions/calc.js
+++ exo-fonctions/calc.js
@@ -32,3 +32,28 @@ const calc = (op, num1, num2) => {
   }
 }
 console.log(calc(sub, 150, 57))
+
+/*
+Méthode switch:
+
+const calc = (operator, nb1, nb2) => {
+  switch (operator) {
+    case '+':
+      return add(nb1, nb2)
+    case '-':
+      return sub(nb1, nb2)
+    case 'x':
+      return mul(nb1, nb2)
+    case '/':
+      return div(nb1, nb2)
+    default:
+      return 'opérateur non connu'
+  }
+}
+
+console.log(calc('-', 10, 2,))
+console.log(calc('+', 10, 2,))
+console.log(calc('x', 10, 2,))
+console.log(calc('%', 10, 2,))
+
+*/
